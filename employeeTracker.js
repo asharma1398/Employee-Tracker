154177eda47a8c4f6e6f6588164d99de41f7edba
asharma1398 <asharma1398@gmail.com>
2020-07-13 18:46:58 -0400

    main page complete and pseudo code updated for user ease of use

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,29 +1,110 @@
 // DEPENDENCIES 
 // =========================================================================
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require('console.table');
+require("dotenv").config();
 
 // NEEDED VARIABLES 
 // =========================================================================
 
 // MYSQL CONNECTION
 // =========================================================================
-
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "employeeTrackerDB"
+  });
+  
 // APPLICATION PAGE FUNCTIONS 
 // =========================================================================
+connection.connect(function(err) {
+    if (err) throw err;
 
-// main page 
+    console.log(`
+    
+        ███████╗███╗   ███╗██████╗ ██╗      ██████╗ ██╗   ██╗███████╗███████╗    
+        ██╔════╝████╗ ████║██╔══██╗██║     ██╔═══██╗╚██╗ ██╔╝██╔════╝██╔════╝    
+        █████╗  ██╔████╔██║██████╔╝██║     ██║   ██║ ╚████╔╝ █████╗  █████╗      
+        ██╔══╝  ██║╚██╔╝██║██╔═══╝ ██║     ██║   ██║  ╚██╔╝  ██╔══╝  ██╔══╝      
+        ███████╗██║ ╚═╝ ██║██║     ███████╗╚██████╔╝   ██║   ███████╗███████╗    
+        ╚══════╝╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝    ╚═╝   ╚══════╝╚══════╝    
+                                                                           
+                                                                                
+           ████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗             
+           ╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗            
+              ██║   ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝            
+              ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗            
+              ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║            
+              ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝            
+                                                                                
+                --- Welcome to the Employee Management System ---     
+    \n`)
+    runMainPage();
+});
+
+// MAIN PAGE 
+function runMainPage() {
+    inquirer
+        .prompt({
+            name: "mainMenuPrompts",
+            type: "list",
+            message: "Please select from the options below:",
+            choices: [
+                "View Data",
+                "Add Data",
+                "Remove Data",
+                "Update Data", 
+                "EXIT"
+            ]
+        })
+        .then(function(response) {
+            switch (response.mainMenuPrompts) {
+                case "View Data":
+                    runViewData();
+                    break;
+                
+                case "Add Data":
+                    runAddData();
+                    break;
+            
+                case "Remove Data":
+                    runRemoveData();
+                    break;
+                
+                case "Update Data":
+                    runUpdateData();
+                    break;
+
+                case "EXIT":
+                    console.log("\n         --- You have successfully exited the Employee Tracker Application! ---\n");
+                    connection.end();
+                    break;
+            }
+        })
+}
+
+// VIEW DATA OPTION
 // view all employees 
-// add employee
-// remove employee - bonus
 // view all roles 
-// add role 
-// remove role - bonus
 // view all departments 
-// add department 
-// remove department – bonus
 // view all employees by department – bonus
 // View all employees by manager – bonus
+// view department’s total utilized budget – bonus 
+
+// ADD DATA OPTION
+// add employee
+// add role 
+// add department 
+
+
+// REMOVE DATA OPTION
+// remove employee - bonus
+// remove role - bonus
+// remove department – bonus
+
+// UPDATE DATA OPTION
 // update employee role
 // update employee manager – bonus 
-// view department’s total utilized budget – bonus 
-// quit 
-
